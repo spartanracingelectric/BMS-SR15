62f7aff07a84baa549fc92d4d63e056361fb18ef
Harinandan Kotamsetti <hareee234@gmail.com>
2024-03-08 17:18:08 -0800

    started working on balancing @MrKarkeys

--- Core/Inc/balance.h
+++ Core/Inc/balance.h
@@ -2,12 +2,13 @@
  * balance.h
  *
  *  Created on: Mar 8, 2024
- *      Author: karth
+ *      Author: hari
  */
 
 #ifndef INC_BALANCE_H_
 #define INC_BALANCE_H_
 
+#include "main.h"
 
 
 #endif /* INC_BALANCE_H_ */
--- Core/Src/balance.c
+++ Core/Src/balance.c
@@ -2,7 +2,38 @@
  * balance.c
  *
  *  Created on: Mar 8, 2024
- *      Author: karth
+ *      Author: hari
  */
 
+#include "balance.h"
+#include <math.h>
+#include "print.h"
+#include "6811.h"
 
+
+
+/**
+ * perform balance
+ * 
+ * @param read_volt array containing cells volts. 
+ * @param length count of readings. 
+ * @param lowest read_volt's lowest cell reading
+*/
+void balance_algorithm(uint16_t *read_volt, uint8_t length, uint16_t lowest) {
+    
+    
+    for (int cell_index = 0; cell_index < length; cell_index++) {
+
+        // check if each cell is close within 50 mV of the lowest cell. 
+
+        if (read_volt[cell_index] - lowest > 50) {
+            // differnce between cell and lowest cell is above 50 mV -- discharge it. 
+
+            // TODO make a struct that contains DCC bits globally changeable. 
+
+
+        }
+
+
+    }
+}
